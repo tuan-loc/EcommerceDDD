18386b564c1e48dbec11b47d09a92d68657a3d80
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-31 11:08:33 +0700

    Added database EF configurations

---
+++ EcommerceDomainDrivenDesign.Infrastructure/Database/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using EcommerceDomainDrivenDesign.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceDomainDrivenDesign.Infrastructure.Database.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products", "dbo");
+
+            builder.HasKey(r => r.Id);
+            builder.Property(e => e.CreationDate);
+            builder.Property(e => e.Name).HasMaxLength(25).IsRequired();
+
+            builder.OwnsOne(e => e.Price, b =>
+            {
+                b.Property(e => e.Value).HasColumnName("Price").HasColumnType("decimal(5,2)");
+                b.OwnsOne(e => e.Currency, bc =>
+                {
+                    bc.Property(e => e.Name).HasColumnName("Currency").HasMaxLength(5).IsRequired();
+                    bc.Property(e => e.Symbol).HasColumnName("CurrencySymbol").HasMaxLength(5).IsRequired();
+                });
+            });
+        }
+    }
+}
